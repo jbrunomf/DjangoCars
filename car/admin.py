79e590fbf8f87b026cc58ff4faa1c42ec0cb2508
João Bruno <jbrunomf@outlook.com>
2025-05-25 20:22:21 -0300

    Update timezone, enhance admin, and refine Car model
    Changed timezone to 'America/Sao_Paulo' to reflect local settings. Added proper admin registration for Car and Brand models with list displays and search fields. Improved the Car model by updating relationships, adding string representation, and refining the value field.

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+from webbrowser import register
 
-# Register your models here.
+from django.contrib import admin
+from car.models import Car, Brand
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['model', 'model_year', 'brand', 'value']
+    search_fields = ('model',)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields =  ('name',)
--- car/models.py
+++ car/models.py
@@ -10,7 +10,10 @@ class Brand(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=100)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='cars')
     manufacturing_year = models.IntegerField()
     model_year = models.IntegerField()
-    value = models.DecimalField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.model
