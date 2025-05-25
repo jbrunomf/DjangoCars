09e4635248b80419710e675c2255148e09a32731
João Bruno <jbrunomf@outlook.com>
2025-05-25 20:01:22 -0300

    Add Brand and Car models to define car properties
    Introduced the Brand model with a name field and string representation. Updated the Car model to include fields for model, brand (foreign key to Brand), manufacturing year, model year, and value. These changes lay the foundation for managing car-related data.

--- car/models.py
+++ car/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Car(models.Model):
-    ...
+    model = models.CharField(max_length=100)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    manufacturing_year = models.IntegerField()
+    model_year = models.IntegerField()
+    value = models.DecimalField()
