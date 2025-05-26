5313db39d644798f88a8eda75e77d72705f4fe9e
João Bruno <jbrunomf@outlook.com>
2025-05-26 00:24:52 -0300

    Add validation for car value and manufacturing year
    Ensure car value meets a minimum of R$ 10000 and restrict manufacturing year to no earlier than 2000. These rules enforce business requirements RN001 and improve data integrity.

--- car/forms.py
+++ car/forms.py
@@ -6,3 +6,20 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    """"
+    Valida se o veículo foi enviado com valor mínimo de R$ 10000,00. (RN001).
+    """
+
+    def clean_value(self):
+        value = self.cleaned_data['value']
+        if value < 10000:
+            self.add_error('value', 'Valor mínimo do veículo deve ser de R$ 10000,00.')
+        return value
+
+    def clean_manufacturing_year(self):
+        year = self.cleaned_data['manufacturing_year']
+
+        if year < 2000:
+            self.add_error('manufacturing_year', 'O ano de fabricação mínimo é 2000.')
+        return year
