a193759041fe855cf292573a0968c224149d2f11
João Bruno <jbrunomf@outlook.com>
2025-06-01 20:07:14 -0300

    Add user login and logout functionality to account app
    Implemented `login_view` and `logout_view` in the `account` app, allowing users to authenticate and end sessions. Updated URLs and templates accordingly to support these features.

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def register_view(request):
@@ -9,5 +10,25 @@ def register_view(request):
             form.save()
             return redirect('login')
     else:
-         form = UserCreationForm()
+        form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login')
+        return render(request, 'login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
