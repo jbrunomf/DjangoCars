cf4fe5c790172ee21505409e25abf445f12ae498
João Bruno <jbrunomf@outlook.com>
2025-06-01 23:40:36 -0300

    Update `CarUpdateView` success URL to use `reverse_lazy`
    Modify `CarUpdateView` to dynamically generate the success URL using `reverse_lazy`, ensuring redirection to the updated car's detail page after form submission.

--- car/views.py
+++ car/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from car.models import Car
@@ -37,6 +38,9 @@ class CarUpdateView(UpdateView):
     form_class = CarModelForm
     template_name = 'car_update.html'
     success_url = '/cars/'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
 
 class CarDeleteView(DeleteView):
