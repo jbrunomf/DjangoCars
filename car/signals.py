cf581a212944983189a7c6d188cb833afdca0d9c
João Bruno <jbrunomf@outlook.com>
2025-06-08 23:04:49 -0300

    Add `car_post_delete` signal and refactor inventory update logic
    - Introduced `car_post_delete` signal to handle inventory updates on car deletion.
    - Refactored `car_post_save` to delegate inventory updates to a shared `car_inventory_update` function.

--- car/signals.py
+++ car/signals.py
@@ -2,7 +2,7 @@ from django.db.models.aggregates import Sum
 from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
 from django.dispatch import receiver
 
-from car.models import CarInventory
+from car.models import CarInventory, Car
 
 
 @receiver(pre_save, sender='car.Car')
@@ -10,13 +10,21 @@ def car_pre_save(sender, instance, *args, **kwargs):
     print("pre_save")
 
 
-@receiver(post_save, sender='car.Car')
-def car_post_save(sender, instance, *args, **kwargs):
-    print('Calculando valor do inventário..')
-    cars = sender.objects.all()
+def car_inventory_update():
+    cars = Car.objects.all()
     total_value = cars.aggregate(Sum('value'))['value__sum'] or 0
     car_count = len(cars)
     CarInventory.objects.create(
         car_count=car_count,
         car_total_value=total_value
     )
+
+
+@receiver(post_save, sender='car.Car')
+def car_post_save(sender, instance, *args, **kwargs):
+    car_inventory_update()
+
+
+@receiver(post_delete, sender='car.Car')
+def car_post_delete(sender, instance, *args, **kwargs):
+    car_inventory_update()
